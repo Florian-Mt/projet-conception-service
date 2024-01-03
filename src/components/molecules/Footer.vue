828974b4fa3d0f46db288d24e45901640807d7aa
Florian Mt <fmonfort@enssat.fr>
2024-01-03 19:36:41 +0100

    Donne un vrai nom et une rédaction hors pair au blogue

--- src/components/molecules/Footer.vue
+++ src/components/molecules/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="p-4">
     <div class="w-full mx-auto max-w-screen-xl p-4 flex flex-col gap-4 items-center md:flex-row md:justify-between bg-white rounded-lg shadow dark:bg-gray-800">
       <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        Mon super blogue
+        Moyen
       </span>
       <ul class="flex flex-col gap-2 items-center sm:flex-row text-sm font-medium text-gray-500 dark:text-gray-400">
         <li>
