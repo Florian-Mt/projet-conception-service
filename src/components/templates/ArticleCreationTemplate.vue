b69b8b455d44171e83c1e1c8494fd889bde07d05
Louis Legrand <louislegrand42@gmail.com>
2024-01-10 19:52:13 +0100

    Accessibilité création d'un page

--- src/components/templates/ArticleCreationTemplate.vue
+++ src/components/templates/ArticleCreationTemplate.vue
@@ -27,9 +27,9 @@
 <template>
 
 <form class="w-full mx-auto max-w-screen-xl p-4 flex flex-col gap-4 grow">
-  <h1 class="self-center font-black text-3xl"> Créer votre post ici</h1>
+  <h1 class="self-center font-black text-3xl" tabindex="0"> Créer votre post ici</h1>
 
-  <p>
+  <p tabindex="0">
     Exprimez-vous, partagez vos idées et découvrez des perspectives variées en participant à nos discussions.
     Cette page dédiée aux posts vous offre l'opportunité d'engager des conversations enrichissantes avec notre communauté diversifiée.
     N'hésitez pas à partager vos expériences, poser des questions, ou simplement échanger des idées.
@@ -38,15 +38,35 @@
 
   <div>
     <h3> Title :</h3>
-    <input type="text" name="title" class="border-2 border-black rounded-lg w-full p-1" required>
+    <input type="text"
+           name="title"
+           class="border-2 border-black rounded-lg w-full p-1"
+           aria-label="title"
+           required>
   </div>
 
   <div>
     <h3> Author :</h3>
-    <input type="text" name="author" class="border-2 border-black rounded-lg w-full p-1" required>
+    <input type="text"
+           name="author"
+           class="border-2 border-black rounded-lg w-full p-1"
+           aria-label="author"
+           required>
   </div>
 
-  <div class="flex flex-row justify-evenly items-center">
+  <div class="flex flex-row-reverse justify-evenly items-center">
+    <div class="w-1/2">
+      <h5 class="font-bold">Note aux utilisateurs :</h5>
+      <p class="font-light italic" tabindex="0">
+        Merci d'utiliser cette plateforme de manière responsable lors de l'upload d'images.
+        Tout contenu téléchargé doit respecter les lois en vigueur et ne pas enfreindre les droits d'auteur.
+        Nous nous réservons le droit de supprimer tout contenu inapproprié ou offensant.
+        En téléchargeant une image, vous confirmez détenir les droits nécessaires ou l'autorisation pour sa publication.
+        Tout manquement à ces conditions pourrait entraîner la suspension de votre compte.
+        Merci de votre compréhension et de votre coopération.
+      </p>
+    </div>
+
     <div class="w-1/6">
 
       <div class="w-full aspect-[1/1] bg-gray-300 border-black border-dashed border-2 rounded-2xl flex flex-col justify-center items-center">
@@ -56,30 +76,26 @@
       <div class="flex flex-row items-center">
         <p class="w-3/12">Url :</p>
         <input @input="onInput"
-               class="w-9/12 m-1 border-black border-2 rounded-lg px-1">
+               class="w-9/12 m-1 border-black border-2 rounded-lg px-1"
+               aria-label="Article image">
       </div>
-    </div>
-
-    <div class="w-1/2">
-      <h5 class="font-bold">Note aux utilisateurs :</h5>
-      <p class="font-light italic">
-        Merci d'utiliser cette plateforme de manière responsable lors de l'upload d'images.
-        Tout contenu téléchargé doit respecter les lois en vigueur et ne pas enfreindre les droits d'auteur.
-        Nous nous réservons le droit de supprimer tout contenu inapproprié ou offensant.
-        En téléchargeant une image, vous confirmez détenir les droits nécessaires ou l'autorisation pour sa publication.
-        Tout manquement à ces conditions pourrait entraîner la suspension de votre compte.
-        Merci de votre compréhension et de votre coopération.
-      </p>
     </div>
   </div>
 
   <div class="flex flex-col grow">
     <h3> Content :</h3>
-    <textarea name="content" class="border-2 border-black rounded-lg p-2 w-full grow" required/>
+    <textarea name="content"
+              class="border-2 border-black rounded-lg p-2 w-full grow"
+              aria-label="Article content"
+              required/>
   </div>
 
   <div class="flex flex-row justify-center gap-5">
-    <input type="submit" value="Envoyer" name="submit" class="w-32 h-10 cursor-pointer rounded-2xl bg-blue-600 text-white">
+    <input type="submit"
+           value="Envoyer"
+           name="submit"
+           class="w-32 h-10 cursor-pointer rounded-2xl bg-blue-600 text-white"
+           aria-label="Send the article">
   </div>
 </form>
 
