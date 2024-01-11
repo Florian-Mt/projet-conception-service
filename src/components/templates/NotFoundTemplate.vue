8392e4795264413b3d200a5e93af040395acabce
Florian Mt <fmonfort@enssat.fr>
2024-01-11 23:43:07 +0100

    Corrige la page 404

--- src/components/templates/NotFoundTemplate.vue
+++ src/components/templates/NotFoundTemplate.vue
@@ -8,10 +8,8 @@
       Cette page n’existe pas.
     </p>
 
-    <router-link to="{name: 'HomePage'}">
-      <a href="#" class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-        Retourner à la page d’accueil
-      </a>
+    <router-link :to="{name: 'HomePage'}" class="inline-flex items-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+      Retourner à la page d’accueil
     </router-link>
   </main>
 </template>
